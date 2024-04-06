b56fe0accd155b4510c787535211267b2d7672c0
emonmahdi <developeremon1997@gmail.com>
2024-04-07 01:32:10 +0600

    fetch the categories done

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+const db = new sqlite3.Database("./dua_main.sqlite", (err) => {
+  if (err) {
+    console.log(err?.message);
+  } else {
+    console.log("Connected to the IRD Dua Database");
+  }
+});
+
+// Endpoint to fetch categories
+app.get("/categories", (req, res) => {
+  db.all("SELECT * FROM category", (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+    console.log(rows);
+    res.json(rows);
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Get the dua from IRD");
