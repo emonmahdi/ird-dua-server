904c8ef6e3fd4b13f718a673763c65e568b588dc
emonmahdi <developeremon1997@gmail.com>
2024-04-07 23:12:19 +0600

    update

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const app = express();
+const cors = require("cors");
 
 const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
+app.use(express.json());
 
 const db = new sqlite3.Database("./dua_main.sqlite", (err) => {
   if (err) {
